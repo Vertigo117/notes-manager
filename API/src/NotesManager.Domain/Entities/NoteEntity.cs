4fe59dd60e8913268adae6e25f849eef1bf76078
avtimche <avtimche@mts.ru>
2022-06-29 09:42:06 +0300

    feat: add domain entities

---
+++ API/src/NotesManager.Domain/Entities/NoteEntity.cs
@@ -0,0 +1,27 @@
+namespace NotesManager.Domain.Entities;
+
+/// <summary>
+/// Заметка
+/// </summary>
+public class NoteEntity : BaseEntity
+{
+    /// <summary>
+    /// Название
+    /// </summary>
+    public string? Name { get; set; }
+
+    /// <summary>
+    /// Текст
+    /// </summary>
+    public string? Text { get; set; }
+
+    /// <summary>
+    /// Дата создания
+    /// </summary>
+    public DateTime CreationDate { get; set; }
+
+    /// <summary>
+    /// Навигационное свойство для связи с сущностью <see cref="UserEntity"/>
+    /// </summary>
+    public UserEntity User { get; set; } = null!;
+}
