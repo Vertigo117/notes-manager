a77b0316304982495e7b61c14bcf70c5c19f7663
avtimche <avtimche@mts.ru>
2022-06-30 10:46:35 +0300

    feat: update summary

--- API/src/NotesManager.Domain/Repositories/IEntityRepository.cs
+++ API/src/NotesManager.Domain/Repositories/IEntityRepository.cs
@@ -10,33 +10,34 @@ namespace NotesManager.Domain.Repositories;
 public interface IEntityRepository<TEntity> where TEntity : BaseEntity
 {
     /// <summary>
-    /// Получить объекты, соответствующие условию
+    /// Асинхронно получает объекты, соответствующие условию
     /// </summary>
     /// <param name="expression">Условие</param>
     /// <returns>Задача, которая содержит коллекцию объектов <typeparamref name="TEntity"/></returns>
     Task<IEnumerable<TEntity>> GetByExpressionAsync(Expression<Func<TEntity, bool>> expression);
 
     /// <summary>
-    /// Получить объект по уникальному идентификатору
+    /// Асинхронно получает объект по уникальному идентификатору
     /// </summary>
     /// <param name="id">Уникальный идентификатор</param>
-    /// <returns>Задача, которая содержит объект с указанным уникальным идентификатором</returns>
+    /// <returns>Задача, которая содержит объект с указанным уникальным идентификатором, либо <c>null</c>,
+    /// если объект не найден</returns>
     Task<TEntity?> GetByIdOrDefaultAsync(int id);
 
     /// <summary>
-    /// Создать новый объект
+    /// Асинхронно создаёт новый объект
     /// </summary>
     /// <param name="entity">Объект для создания</param>
     void Create(TEntity entity);
 
     /// <summary>
-    /// Обновить информацию о существующем объекте
+    /// Обновляет информацию об объекте
     /// </summary>
     /// <param name="entity">Объект для обновления</param>
     void Update(TEntity entity);
 
     /// <summary>
-    /// Удалить объект
+    /// Удаляет объект
     /// </summary>
     /// <param name="entity">Объект для удаления</param>
     void Delete(TEntity entity);
--- API/src/NotesManager.Storage.PostgreSql/DependencyInjection.cs
+++ API/src/NotesManager.Storage.PostgreSql/DependencyInjection.cs
@@ -7,18 +7,23 @@ using NotesManager.Storage.PostgreSql.Repositories;
 
 namespace NotesManager.Storage.PostgreSql;
 
+/// <summary>
+/// Методы расширений для инъекции зависимостей
+/// </summary>
 public static class DependencyInjection
 {
     /// <summary>
-    /// Зарегистрировать зависимости для работы с бд
+    /// Регистрирует зависимости для работы с бд
     /// </summary>
     /// <param name="services"><see cref="IServiceCollection"/> для регистрации сервисов</param>
-    /// <param name="configuration"><see cref="IConfiguration"/> для работы с конфигурацией проекта</param>
+    /// <param name="configuration"><see cref="IConfiguration"/> для доступа к конфигурации проекта</param>
     public static void AddPostgreSqlStorage(this IServiceCollection services, IConfiguration configuration)
     {
         var connectionString = configuration.GetConnectionString(nameof(NotesManagerDbContext));
+        
         services.AddDbContextPool<NotesManagerDbContext>(builder => builder.UseNpgsql(connectionString!,
-            c => c.MigrationsHistoryTable("__EFMigrationsHistory", NotesManagerDbContext.SchemaName)));
+            npgBuilder =>
+                npgBuilder.MigrationsHistoryTable("__EFMigrationsHistory", NotesManagerDbContext.SchemaName)));
 
         services.AddScoped<IEntityRepository<NoteEntity>, NoteEntityRepository>();
         services.AddScoped<IEntityRepository<UserEntity>, UserEntityRepository>();
